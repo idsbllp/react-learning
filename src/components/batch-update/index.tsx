b008e6ec48e35ebd379e4810f17b590d016b7517
idsbllp <idsbllp@gmail.com>
2022-02-23 14:14:40 +0800

    feat: add batch update demo

---
+++ src/components/batch-update/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { unstable_batchedUpdates } from 'react-dom';
+
+interface IState {
+  count: number;
+}
+
+class BatchedUpdate extends React.PureComponent<{}, IState> {
+  state: Readonly<IState> = {
+    count: 0,
+  };
+
+  private handleAsyncClick = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+    this.setState({
+      count: this.state.count + 1,
+    });
+  }
+
+  handleSyncClick = () => {
+    setTimeout(() => {
+      this.handleAsyncClick();
+    }, 0);
+  }
+
+  private handleBatchUpdateClick = () => {
+    setTimeout(() => {
+      unstable_batchedUpdates(() => {
+        this.handleAsyncClick();
+      });
+    }, 0);
+  }
+
+
+  render(): React.ReactNode {
+    const { count } = this.state;
+    return (
+      <div>
+        <h1>点击了几次:  {count}</h1>
+        <button onClick={this.handleAsyncClick}>同步</button>
+        <button onClick={this.handleSyncClick}>异步</button>
+        <button onClick={this.handleBatchUpdateClick}>Batched</button>
+      </div>
+    );
+  }
+}
+
+export default BatchedUpdate;
